a8f563817ba4f2001fa6f0e274a0302a43c171c5
iamgowri <gowrishankari27800@gmail.com>
2024-04-19 17:23:27 +0530

    35:20

--- deliverooClone/Components/CustomHeader.tsx
+++ deliverooClone/Components/CustomHeader.tsx
@@ -2,6 +2,25 @@ import { View, Text, SafeAreaView,StyleSheet,TouchableOpacity,Image} from 'react
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
+import { Link } from 'expo-router';
+import { TextInput } from 'react-native-gesture-handler';
+
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons style={styles.searchIcon} name="ios-search" size={20} color={Colors.medium}/>
+        <TextInput style={styles.input}placeholder="Restaurants, groceries, dishes"/>
+    </View>
+    <Link href={'/'} asChild>
+      <TouchableOpacity style={styles.optionButton}>
+        <Ionicons name="options-Outline" size={20} color={Colors.primary}/>
+      </TouchableOpacity>
+      </Link>
+    </View>
+    </View>
+  
+)
 
 const CustomHeader = () => {
   return (
@@ -12,7 +31,11 @@ const CustomHeader = () => {
      </TouchableOpacity>
 
      <TouchableOpacity style={styles.titleContainer}>
-      <Text>Home</Text>
+      <Text>Delivery.Now</Text>
+      <View style={styles.locationName}>
+        <Text style={styles.subtitle}>London</Text>
+        <Ionicons name="chevron-down" size={20} color={Colors.primary}/>
+        </View>
      </TouchableOpacity>
 
      <TouchableOpacity style={styles.profileButton}>
@@ -20,6 +43,7 @@ const CustomHeader = () => {
       {/* <Text>Home</Text> */}
      </TouchableOpacity>
       </View>
+      <SearchBar></SearchBar>
     </SafeAreaView>
   );
 };
@@ -47,7 +71,62 @@ const styles = StyleSheet.create({
   titleContainer: {
     flex:1,
   },
-  profileButton:{},
+  title:{
+    fontSize:14,
+    color: Colors.medium,
+  },
+
+  locationName: {
+    flexDirection:'row', 
+    alignItems:'center'},
+
+  subtitle:{
+     fontSize: 18,
+     fontWeight: 'bold',
+  },
+  profileButton:{
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  searchContainer: {
+  height: 60,
+  backgroundColor: '#fff', 
+  },
+
+     
+
+searchSection:{
+  flexDirection:'row',
+  gap:10,
+  flex:1,
+  paddingHorizontal:20,
+  alignItems:'center',
+},
+searchField:{
+  flex:1,
+  backgroundColor: Colors.lightGrey,
+  borderRadius:8,
+  flexDirection:'row',
+  alignItems:'center',
+},
+input:{
+  padding:10,
+  color:Colors.mediumDark,
+},
+searchIcon:{
+   paddingLeft:10,
+},
+optionButton:{
+    padding:10,
+    borderRadius:8,
+
+},
+
+
 });
+
+
+
 
 export default CustomHeader;
