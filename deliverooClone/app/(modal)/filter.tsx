03efc273c44d585e3a0b8391c1b64c06894c4334
iamgowri <gowrishankari27800@gmail.com>
2024-04-23 11:30:10 +0530

    bug fixed

--- deliverooClone/app/(modal)/filter.tsx
+++ deliverooClone/app/(modal)/filter.tsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Colors from '@/constants/Colors';
+
 
 const filter = () => {
   return (
-    <View>
+    <View style = {styles.container}>
       <Text>filter</Text>
-    </View>
-  )
-}
 
-export default filter
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container:{
+    flex: 1,
+    padding:24,
+    backgroudColor:Colors.lightGrey
+    },
+    footer:{
+      position:'absolute',
+      bottom:0,
+      left:0,
+      right:0,
+      height:100,
+    }
+  });
+
+  export default filter;
+
+  
+
+
